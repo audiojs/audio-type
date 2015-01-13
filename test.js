708fd9defd24fe1a5172c6d9a639ea6e4da32cd9
Hemanth.HM <hemanth.hm@gmail.com>
2015-01-13 20:42:59 +0530

    Dep ver bump & test desc.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ function check(filename) {
 	return audioType(readChunk.sync(filename, 0, 12));
 }
 
-it('should detect image type from Buffer', function () {
+it('should detect audio type from Buffer', function () {
 	assert.strictEqual(check('fixture.mp3'), 'mp3');
 	assert.strictEqual(check('fixture.wav'), 'wav');
   assert.strictEqual(check('fixture.png'), false);
