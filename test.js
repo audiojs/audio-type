09a62c447b812a7cd525d3310f6b71b57d288d71
Dmitry <df.creative@gmail.com>
2017-05-02 01:07:34 -0400

    Replace mocha with lightweight tape

--- test.js
+++ test.js
@@ -2,13 +2,15 @@
 var assert = require('assert');
 var readChunk = require('read-chunk');
 var audioType = require('./index');
+var test = require('tape')
 
 function check(filename) {
 	return audioType(readChunk.sync(filename, 0, 12));
 }
 
-it('should detect audio type from Buffer', function () {
+test('should detect audio type from Buffer', function (t) {
 	assert.strictEqual(check('fixture.mp3'), 'mp3');
 	assert.strictEqual(check('fixture.wav'), 'wav');
-  assert.strictEqual(check('fixture.png'), false);
+	assert.strictEqual(check('fixture.png'), false);
+	t.end()
 });
